2aac44f9f906b3aced31e0bc4541c5b69c290db1
Seckin Cengiz <4427409+seckincengiz@users.noreply.github.com>
2024-08-28 01:52:46 +0300

    change style

--- docs/static/js/main.a0b83190.chunk.js
+++ docs/static/js/main.a0b83190.chunk.js
@@ -224,7 +224,7 @@
                         children: Object(l.jsxs)("div", {
                             className: "intro",
                             children: [Object(l.jsx)(d.a, {
-                                type: "",
+                                type: "box",
                                 bg: {
                                     zIndex: 0,
                                     position: "absolute",
