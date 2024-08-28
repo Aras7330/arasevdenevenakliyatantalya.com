45609d86085b962f49e998160ef48cd61f9e0038
Seckin Cengiz <4427409+seckincengiz@users.noreply.github.com>
2024-08-28 03:26:29 +0300

    change button

--- docs/static/js/main.a0b83190.chunk.js
+++ docs/static/js/main.a0b83190.chunk.js
@@ -245,7 +245,7 @@
                                             }), Object(l.jsx)("a", {
                                                 href: "#features",
                                                 className: "btn btn-custom btn-lg page-scroll",
-                                                children: "Daha fazla bilgi"
+                                                children: "DAHA FAZLA BİLGİ"
                                             }), " "]
                                         })
                                     })
